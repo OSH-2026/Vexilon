a4d6e25bef722f1a7877438428c380f85792afef
XY23zmds46 <3120310177@qq.com>
2026-05-18 15:04:11 +0800

    update rust code

--- LiteOS-M/rust_kernel/target/debug/build/libc-f4def49aa4b814e4/build_script_build-f4def49aa4b814e4.d
+++
@@ -1,5 +0,0 @@
-\\wsl.localhost\Ubuntu\home\lenovo\Desktop\Vexilon\LiteOS-M\rust_kernel\target\debug\build\libc-f4def49aa4b814e4\build_script_build-f4def49aa4b814e4.d: C:\Users\Lenovo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.186\build.rs
-
-\\wsl.localhost\Ubuntu\home\lenovo\Desktop\Vexilon\LiteOS-M\rust_kernel\target\debug\build\libc-f4def49aa4b814e4\build_script_build-f4def49aa4b814e4.exe: C:\Users\Lenovo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.186\build.rs
-
-C:\Users\Lenovo\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\libc-0.2.186\build.rs:
